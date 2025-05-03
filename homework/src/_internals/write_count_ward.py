26bf5ea72e36301dc2a5b6d92bb5a0f38549bef1
NicoleVargasOssa <nvargaso@unal.edu.co>
2025-05-03 11:19:30 -0500

    input_output_folder

--- homework/src/_internals/write_count_ward.py
+++ homework/src/_internals/write_count_ward.py
@@ -1,12 +1,12 @@
 import os
 
 
-def write_count_ward(counter):
-    if not os.path.exists("data/output"):
-        os.makedirs("data/output")
+def write_count_ward(counter, output_folder):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
-    # save the results using tsv format
-    with open("data/output/results.tsv", "w", encoding="utf-8") as f:
+    out_path = os.path.join(output_folder, "results.tsv")
+    with open(out_path, "w", encoding="utf-8") as f:
         for key, value in counter.items():
             # write the key and value to the file
             f.write(f"{key}\t{value}\n")
