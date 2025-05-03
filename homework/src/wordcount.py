12366a2f328e7b0835dc8aaf466881d4a1aaa193
NicoleVargasOssa <nvargaso@unal.edu.co>
2025-05-03 10:45:17 -0500

    funciones_archivos_cadauna

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,32 +1,25 @@
 # obtain a list of files in the input directory
-import os
 
 from ._internals.write_count_ward import write_count_ward
+from .count_words import count_words
+from .preprocess_lines import preprocess_lines
+from .read_all_lines import read_all_lines
+from .split_in_words import split_in_words
 
 
 def main():
 
     ## mover a la funcion "read_all_lines"
-    all_lines = []
-    input_file_list = os.listdir("data/input/")
-    for filename in input_file_list:
-        file_path = os.path.join("data/input", filename)
-        with open(file_path, "r", encoding="utf-8") as f:
-            lines = f.readlines()
-            all_lines.extend(lines)
+    all_lines = read_all_lines()
 
     ## mover a "preprocess_lines"
-    all_lines = [line.lower().strip() for line in all_lines]
+    all_lines = preprocess_lines(all_lines)
 
     ## mover "split_in_words"
-    words = []
-    for line in all_lines:
-        words.extend(word.strip(",.!?") for word in line.split())
+    words = split_in_words(all_lines)
 
     ## mover a "count_words"
-    counter = {}
-    for word in words:
-        counter[word] = counter.get(word, 0) + 1
+    counter = count_words(words)
 
     # count the frequency of the words in the files in the input directory
     # counter = {}
