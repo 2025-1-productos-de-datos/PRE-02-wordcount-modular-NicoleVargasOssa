1ac8162dad3c3f6018919cb3013504a67a5bc26d
NicoleVargasOssa <nvargaso@unal.edu.co>
2025-05-03 12:29:09 -0500

    final_diosayuda

--- homework/src/_internals/write_word_counts.py
+++ homework/src/_internals/write_word_counts.py
@@ -1,11 +1,12 @@
 import os
 
 
-def write_count_ward(counter, output_folder):
+def write_count_words(counter, output_folder):
+
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    out_path = os.path.join(output_folder, "results.tsv")
+    out_path = os.path.join(output_folder, "wordcount.tsv")
     with open(out_path, "w", encoding="utf-8") as f:
         for key, value in counter.items():
             # write the key and value to the file
